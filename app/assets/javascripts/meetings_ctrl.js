1d641eb4c745394daaee868b89dfd712b3545dd1
Peter Jang <peter.x.jang@gmail.com>
2016-04-26 17:01:55 -0500

    buttons to filter by tags

--- app/assets/javascripts/meetings_ctrl.js
+++ app/assets/javascripts/meetings_ctrl.js
@@ -5,6 +5,34 @@
       $http.get('/api/v1/meetings.json').then(function(response) {
         $scope.meetings = response.data;
       });
+      $http.get('/api/v1/tags.json').then(function(response) {
+        $scope.tags = response.data;
+      });
+    };
+
+    $scope.filterByTag = function(tag) {
+      $scope.tagFilterId = tag.id;
+    };
+
+    $scope.resetFilterByTag = function() {
+      $scope.tagFilterId = null;
+    };
+
+    $scope.matchTag = function(meeting) {
+      if ($scope.tagFilterId) {
+        // Use a for loop to see if $scope.tagFilterId exists in meeting.tags
+        for (var i = 0; i < meeting.tags.length; i++) {
+          var tag = meeting.tags[i];
+          if (tag.id === $scope.tagFilterId) {
+            return true;
+          }
+        }
+        return false;
+        // Or accomplish the same thing in one line:
+        // return meeting.tags.map(tag => tag.id).indexOf($scope.tagFilterId) !== -1;
+      } else {
+        return true;
+      }
     };
   });
 })();
