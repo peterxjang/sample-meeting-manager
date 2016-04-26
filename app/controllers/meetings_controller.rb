04abb6025c0836aee572895b810566de1dc3029a
Peter Jang <peter.x.jang@gmail.com>
2016-04-26 13:44:45 -0500

    meeting new and create actions

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -8,6 +8,26 @@ class MeetingsController < ApplicationController
     render 'index.html.erb'
   end
 
+  def new
+    @meeting = Meeting.new
+    render 'new.html.erb'
+  end
+
+  def create
+    @meeting = Meeting.new(
+      name: params[:name],
+      address: params[:address],
+      start_time: params[:start_time],
+      end_time: params[:end_time],
+      notes: params[:notes]
+    )
+    if @meeting.save
+      redirect_to '/meetings'
+    else
+      render 'new.html.erb'
+    end
+  end
+
   def show
     @meeting = Meeting.find_by(id: params[:id])
     render 'show.html.erb'
