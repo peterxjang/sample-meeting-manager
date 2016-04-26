3dbe213bcb310fd1ce7c17d2d0e72ba643de2664
Peter Jang <peter.x.jang@gmail.com>
2016-04-26 16:12:46 -0500

    use angular instead of rails to display meetings in index view

--- app/assets/javascripts/meetings_ctrl.js
+++ app/assets/javascripts/meetings_ctrl.js
@@ -3,7 +3,7 @@
   angular.module('app').controller('meetingsCtrl', function($scope, $http) {
     $scope.setup = function() {
       $http.get('/api/v1/meetings.json').then(function(response) {
-        console.log(response);
+        $scope.meetings = response.data;
       });
     };
   });
