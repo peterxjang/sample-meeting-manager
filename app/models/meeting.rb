72fc7b45737a643b9d266839c82665841599b3ba
Peter Jang <peter.x.jang@gmail.com>
2016-04-26 13:56:13 -0500

    meeting validations

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,4 +1,9 @@
 class Meeting < ActiveRecord::Base
   has_many :meeting_tags
   has_many :tags, through: :meeting_tags
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
 end
