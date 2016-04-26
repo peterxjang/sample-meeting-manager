ec01ece597c41c2b2932093520abdd6298f8c322
Peter Jang <peter.x.jang@gmail.com>
2016-04-26 16:07:30 -0500

    get api data using

--- app/assets/javascripts/meetings_ctrl.js
+++ app/assets/javascripts/meetings_ctrl.js
@@ -1,6 +1,10 @@
 /* global angular */
 (function() {
   angular.module('app').controller('meetingsCtrl', function($scope, $http) {
-    $scope.message = "hello";
+    $scope.setup = function() {
+      $http.get('/api/v1/meetings.json').then(function(response) {
+        console.log(response);
+      });
+    };
   });
 })();
